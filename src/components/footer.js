43a0506a3b08dbafa477105385b0a1cf9ab85e58
n2oliver <silva.liver@gmail.com>
2026-07-22 17:46:33 -0300

    adiciona anúncio aads

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,10 @@ import '../css/footer.css';
 import '../css/gtranslate.css';
 import SocialMedia from './social-media.js';
 import '../js/gtranlate-settings.js';
+import AAdsSticky from './aads-sticky.js';
 
 function Footer() {
-    return (
+    return (<>
   <footer className="site-footer mt-2 m-auto">
     <a href="/"><span className="oliver-dev-logo footer-logo">n2oliver</span></a>
     <div className="d-flex justify-content-center">
@@ -12,7 +13,7 @@ function Footer() {
     </div>
     
     <span className="footer-text-small">
-      Todos os direitos reservados - n2oliver - 2025
+      Todos os direitos reservados - n2oliver - 2026
     </span>
 
     <a href="/politica-de-privacidade.html">Política de Privacidade</a>
@@ -20,6 +21,7 @@ function Footer() {
     <span>
       <a href="mailto:suporte@n2oliver.com" style={{marginLeft: "10px"}}>suporte@n2oliver.com</a>
     </span>
-  </footer>)
+  </footer>
+    <AAdsSticky/></>)
 }
 export default Footer;
